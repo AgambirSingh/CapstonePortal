2f169354004caaf3e21d4a42a73d6ae851679416
Agambir Singh <sinagamb@sheridancollege.ca>
2025-04-05 09:29:42 -0400

    Initial commit: Set up CapstonePortal project structure

---
+++ capstone-portal/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Welcome to Capstone Portal!</h1>
+            <p>Your one-stop solution for managing Sheridan College's capstone competition.</p>
+        </div>
+    );
+}
+
+export default App;
